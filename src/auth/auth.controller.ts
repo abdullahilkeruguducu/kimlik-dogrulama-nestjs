4119c288c713240b84d72a592000dd77bf100405
Abdullah Ilker UGUDUCU <abdullahilkeruguducu@gmail.com>
2023-08-11 18:24:27 +0300

    prisma schemasi olusturuldu db baglantisi yapildi.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+constructor(private authService: AuthService) {}
+
+@Post('signup')
+signup(@Body()dto:AuthDto)
+}
